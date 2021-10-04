6d099c97f28f58148dff77d0fb7cbc77c3905e58
lalancetted <daniel_lalancette@outlook.com>
2021-10-04 17:11:39 -0400

    Color => LED

--- lib/RGBLedManager/src/RGBLedManager.cpp
+++ lib/RGBLedManager/src/RGBLedManager.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include "RGBLedManager.h"
 #include "Color.h"
 
@@ -10,29 +11,64 @@ RGBLedManager::RGBLedManager()
 
 // NOTE: les couleurs sont présentes pour les tests.
 Color RGBLedManager::getColor(int nbPM25)
+=======
+<<<<<<< Updated upstream
+=======
+#include "RGBLedManager.h"
+
+RGBLedManager::RGBLedManager(int pinR, int pinG, int pinB)
+{
+    Serial.begin(9600);
+    ledcAttachPin(pinR, 1); 
+    ledcAttachPin(pinG, 2);
+    ledcAttachPin(pinB, 3);
+}
+
+// NOTE: les couleurs sont présentes pour les tests.
+LED RGBLedManager::getColor(int nbPM25)
+>>>>>>> Stashed changes
 {
     if (nbPM25 <= 11)
     {
         ledcWrite(1, 0);
         ledcWrite(2, 255);
+<<<<<<< Updated upstream
         return Color::GREEN;
+=======
+        return LED::GREEN;
+>>>>>>> Stashed changes
     }
     if (nbPM25 >= 12 && nbPM25 <= 34) 
     {
         ledcWrite(1, 255);
         ledcWrite(2, 255);
+<<<<<<< Updated upstream
         return Color::YELLOW;
+=======
+        return LED::YELLOW;
+>>>>>>> Stashed changes
     }
     if (nbPM25 >= 35 && nbPM25 <= 53) 
     {
         ledcWrite(1, 255);
         ledcWrite(2, 127);
+<<<<<<< Updated upstream
         return Color::ORANGE;
+=======
+        return LED::ORANGE;
+>>>>>>> Stashed changes
     }
     if (nbPM25 >= 55) 
     {
         ledcWrite(1, 255);
         ledcWrite(2, 0);
+<<<<<<< Updated upstream
         return Color::RED;
     }
 }
+=======
+        return LED::RED;
+    }
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
--- lib/RGBLedManager/src/RGBLedManager.h
+++ lib/RGBLedManager/src/RGBLedManager.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include <Arduino.h>
 #include "Color.h"
 class RGBLedManager{
@@ -8,3 +9,18 @@ class RGBLedManager{
         uint8_t ledR = 12;  
         uint8_t ledG = 13;
 };
+=======
+<<<<<<< Updated upstream
+=======
+#include <Arduino.h>
+#include "LED.h"
+class RGBLedManager{
+    public:
+        RGBLedManager(int pinR, int pinG, int pinB);
+        LED getColor(int nbPM25);
+    private:
+        uint8_t ledR = 12;  
+        uint8_t ledG = 13;
+};
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
